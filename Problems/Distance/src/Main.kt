3db6ac5cb309874201a03a30b87663a5a01b002c
Commissar94 <andreykomissarov94@gmail.com>
2021-03-12 14:11:39 +0300

    2 problems solved

---
+++ Problems/Distance/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val distance = readLine()!!.toDouble()
+    val hours = readLine()!!.toInt()
+
+    println(distance / hours)
+}
---
+++ Problems/Liquid pressure/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val density = readLine()!!.toDouble()
+    val hight = readLine()!!.toDouble()
+    val gravity = 9.8
+
+    println(density * hight * gravity)
+
+}
